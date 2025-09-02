4d114943710e9713e2674f3f7904c9e0e06ed6f7
user <user@admin.admin>
2025-09-02 12:25:11 -0400

    ajout des fichiers .py

---
+++ TP1/fonctions.py
@@ -0,0 +1,5 @@
+def puissance(a,b):
+ if not type(a) is int:
+  raise TypeError ("erreur")
+ else:
+  return(a**b)
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,8 @@
-print ("Hello World")
+import fonctions as f
+
+
+while True:
+          a = int(input("taper un nombre"))
+          b = int(input("taper un nombre")) 
+          res = f.puissance(a,b)
+          print(res)
