aefb89d5a51fc32c055a0f0cbf157d9f6894bbcb
fa25-letou@IUTR-25-695b <antoine.letourneur@univ-rouen.fr>
2025-09-04 16:47:55 +0200

    vrm pls

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,16 +1,10 @@
-def test_exc_1():
-    with pytest.raises(Exception):
-        puissance(0,-1)
-
-def test_exc_2():
-    with pytest.raises(Exception):
-        puissance(0,b)
-
 def puissance(a, b):
     if not isinstance(a, int):
         raise TypeError("Erreur : 'a' doit être un entier")
     if not isinstance(b, int):
         raise TypeError("Erreur : 'b' doit être un entier")
+    if a == 0 and b < 0:
+    raise Exception ("pas possible si = 0 ou <0)
 
     result = 1
 
