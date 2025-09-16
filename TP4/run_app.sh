223caec0853b89eaa53d17c0356a7e1ed23241f8
user <user@admin.admin>
2025-09-16 09:01:48 -0400

    yes

--- TP4/run_app.sh
+++ TP4/run_app.sh
@@ -6,7 +6,7 @@ docker run -d \
     --network net-tp4 \
     -v "$(pwd)/TP4/srv/ " \
     -p 5000:5000 \
-    im-tp4.2
+    im-tp4
 
 if [ $? -eq 0 ]; then
     echo " Conteneur tp4-app lancé avec succès sur http://localhost:5000"
