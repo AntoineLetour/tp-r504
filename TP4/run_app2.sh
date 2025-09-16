6c0d7741778f8c19c3283a384720812eee78dce5
user <user@admin.admin>
2025-09-16 09:05:44 -0400

    yes

---
+++ TP4/run_app2.sh
@@ -0,0 +1,14 @@
+echo " Lancement du conteneur tp4-app à partir de l'image im-tp4..."
+docker run -d \
+    --name tp4-app2 \
+    --network net-tp4 \
+    -v "$(pwd)/srv/:/srv/" \
+    -p 5000:5000 \
+    im-tp4.2
+
+if [ $? -eq 0 ]; then
+    echo " Conteneur tp4-app lancé avec succès sur http://localhost:5000"
+else
+    echo " Échec du lancement du conteneur."
+    exit 1
+fi
---
+++ TP4/srv/app_1.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+ 
+# MySQL configuration
+db_config = {
+    'host': 'tp4-sql',
+    'user': 'root',
+    'password': 'foo',
+    'database': 'demosql'
+}
+@app.route('/')
+def index():
+    try:
+        # Connexion MySQL à chaque requête
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        
+        query = "SELECT * FROM myTable"
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    finally:
+        cursor.close()
+        conn.close()
+    
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')  # important pour Docker
+
